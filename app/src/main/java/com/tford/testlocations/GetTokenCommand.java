aa4388562c60499392151ee807fc04f16f845d37
Terry Ford <tford@sfo-m-2352.corp.fitbit.com>
2016-12-30 15:13:09 -0800

    Initial commit from AndroidStudio generated project.

---
+++ app/src/main/java/com/tford/testlocations/GetTokenCommand.java
@@ -0,0 +1,74 @@
+package com.tford.testlocations;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * Created by tford on 12/30/16.
+ */
+
+public class GetTokenCommand {
+    private static final String url = "https://localapi.fitbit.com/oauth2/token";
+    private static final String auth = "Basic MjI4VFNMOjYxNzlrb3B5b3hkcTFzYngyeGpxa3Uycnc2bnhtajg1";
+
+    public static String execute(OkHttpClient client) {
+        Log.d("GetTokenCommand.execute", "Inside GetTokenCommand.execute, got called!!!");
+        Request request = createRequest();
+        Response response = null;
+        String token = null;
+        try {
+            Call newCall = client.newCall(request);
+            response = newCall.execute();
+            token = extractToken(response);
+        } catch(IOException e) {
+            Log.wtf("GetTokenCommand.execute", e);
+        } catch(Exception e) {
+            Log.wtf("GetTokenCommand.execute", e);
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return token;
+    }
+
+    private static Request createRequest() {
+        Request.Builder requestBuilder = new Request.Builder();
+        FormBody.Builder formBodyBuilder = new FormBody.Builder();
+        RequestBody formBody = formBodyBuilder
+                .add("grant_type", "password")
+                .add("username", "ttf0809@gmail.com")
+                .add("password", "Fu8a$hank")
+                .build();
+        requestBuilder.url(url)
+                .header("Authorization", auth)
+                /*
+                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"),
+                        "{grant_type:\"password\", email:\"ttf0809@gmail.com\", password:\"Fu8a$hank\"}"
+                ));
+                */
+                //.post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), "grant_type=password&email=ttf08"));
+                .post(formBody);
+        Request request = requestBuilder.build();
+        return request;
+    }
+
+    private static String extractToken(Response response) {
+        ResponseBody responseBody = response.body();
+        if (responseBody == null) {
+            throw new RuntimeException(String.format("Could not get body of response"));
+        }
+        Log.d("GetTokenCommand.extractToken", "The response body looks like" + responseBody.toString());
+        return "poopytoken";
+    }
+}
---
+++ app/src/main/java/com/tford/testlocations/MainActivity.java
@@ -0,0 +1,87 @@
+package com.tford.testlocations;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.security.KeyManagementException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by tford on 12/30/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    private OkHttpClient insecureClient;
+    private String requestToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getToken(View view) {
+        System.out.printf("Inside MainActivity.getToken, got called...");
+        System.out.printf("Here is where we need to make a request for a token!!!");
+        Log.d("MainActivity.getToken", "Got called...Here is where we should make a request for the token...");
+        GetTokenTask getTokenTask = new GetTokenTask();
+        getTokenTask.execute();
+    }
+
+    private class GetTokenTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            Log.d("MainActivity.GetTokenTask.doInBackground", "Inside GetTokenTask.doInBackground, got called!!!");
+            String token = GetTokenCommand.execute(getInsecureClient());
+            return token;
+        }
+
+        @Override
+        protected void onPostExecute(String token) {
+            if (token != null) {
+                requestToken = token;
+                TextView tokenResponseTextView = (TextView) findViewById(R.id.token_response);
+                tokenResponseTextView.setText(requestToken);
+            }
+        }
+    }
+
+    private OkHttpClient getInsecureClient() {
+        if (insecureClient != null) {
+            return insecureClient;
+        }
+
+        X509TrustManager[] insecureTrustManagers = new X509TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+                }
+        };
+        SSLSocketFactory insecureSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, insecureTrustManagers, null);
+            insecureSocketFactory = sslContext.getSocketFactory();
+        } catch (KeyManagementException e) {
+            Log.wtf("help key management", e);
+        } catch (Exception e) {
+            Log.wtf("HELP SOME OTHER EXCEPTION!!!", e);
+        }
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient().newBuilder();
+        clientBuilder.sslSocketFactory(insecureSocketFactory, insecureTrustManagers[0]);
+        insecureClient = clientBuilder.build();
+        return insecureClient;
+    }
+}
